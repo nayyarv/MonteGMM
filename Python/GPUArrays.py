7b95051d18b8b4bae23f18b3ffc4478c711bb1c2
varunnayyar <nayyarv@gmail.com>
2014-10-17 04:58:16 +1100

    fixes

--- Python/GPUArrays.py
+++ Python/GPUArrays.py
@@ -27,7 +27,7 @@ means_gpu = numGen.gen_normal(shape=(int(numMixtures),int(dim)), dtype = np.floa
 diagCovs_gpu = numGen.gen_uniform(shape=(int(numMixtures),int(dim)), dtype = np.float32)+1
 
 weights_gpu = numGen.gen_uniform(shape=(int(numMixtures),int(dim)), dtype = np.float32)
-print type(gpuarray.sum(weights))
+print type(gpuarray.sum(weights_gpu))
 # weights_gpu=  
 
 emptyLikelihood_gpu = gpuarray.zeros(shape = int(numPoints))
